878fc293d483ae0f29fe2ffe127b9c0d47531bda
rioaurul2 <pmarin08@yahoo.com>
2024-01-06 23:45:05 +0200

    DutchNationalFlagAlgoritmh

---
+++ Sirul lui fibonacii/Sirul lui fibonacii/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sirul_lui_fibonacii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var fibonaci = new List<long> {1,1};
+            int fibonaciLenght = 20;
+            for (int i = 1; i < fibonaciLenght-1; i++)
+            {
+                fibonaci.Add(fibonaci[i] + fibonaci[i - 1]);
+
+            }
+            foreach (int number in fibonaci)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ TheDutchNationalFlagAlgorithm/Sort012sArray/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arrayNumbers = { 0, 1, 2, 1, 0, 2, 1, 2, 0, 1, 1 };
+
+        //Console.WriteLine("This array is sorted by the build in 'Array.Sort()' method");
+
+        //Array.Sort(arrayNumbers);
+
+        //for(int i = 0; i < arrayNumbers.Length; i++)
+        //{
+        //    Console.Write(arrayNumbers[i]+ " ");
+        //}
+
+        for (int i = 0; i < arrayNumbers.Length; i++)
+        {
+            Console.Write(arrayNumbers[i] + " ");
+        }
+
+        Console.WriteLine("");
+
+        Sort012SArray(arrayNumbers);
+
+        PrintArray(arrayNumbers);
+
+    }
+    static void Sort012SArray(int[] array)
+    {
+        int low = 0;
+        int high = array.Length - 1;
+        int mid = 0;
+
+        while (mid <= high)
+        {
+            switch (array[mid])
+            {
+                case 0:
+                    Swap(array, low, mid);
+                    low++;
+                    mid++;
+
+                    break;
+                case 1:
+                    mid++;
+
+                    break;
+                case 2:
+                    Swap(array, mid, high);
+                    high--;
+
+                    break;
+            }
+        }
+    }
+
+    static void Swap(int[] array, int i, int j)
+    {
+        int container = array[i];
+        array[i] = array[j];
+        array[j] = container;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (var element in array)
+        {
+            Console.Write(element + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+}
